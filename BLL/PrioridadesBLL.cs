10650dccdfc6b6e1ac1fdd3487aa6afd34c9ec99
Yudelka <yudelkaguillen07@gmail.com>
2024-01-11 20:10:24 -0400

    Agregué otros métodos.

--- BLL/PrioridadesBLL.cs
+++ BLL/PrioridadesBLL.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore.Metadata;
 using ProyectoManager.DAL;
 using ProyectoManager.Models;
+using System.Linq.Expressions;
 
 namespace ProyectoManager.BLL
 {
@@ -30,6 +31,35 @@ namespace ProyectoManager.BLL
             _contexto.Update(prioridades);
             int cant = _contexto.SaveChanges();
             return cant > 0;
+        }
+        
+        public bool Exist(int prioridadesId)
+        {
+            return _contexto.Prioridades.Any(o => o.PrioridadesId == prioridadesId);
+
+        }
+
+        private bool Insert(Prioridades prioridades)
+        {
+            _contexto.Prioridades.Add(prioridades);
+            return _contexto.SaveChanges() > 0;
+        }
+
+        public bool Eliminar(Prioridades prioridades)
+        {
+            _contexto.Entry(prioridades).State = EntityState.Deleted;
+            return _contexto.SaveChanges() > 0;
+        }
+
+        public Prioridades? Buscar(int prioridadesId)
+        {
+            return _contexto.Prioridades.Where(o => o.PrioridadesId
+            == prioridadesId).AsNoTracking().SingleOrDefault();
+        }
+
+        public List<Prioridades> GetList(Expression<Func<Prioridades, bool>> criterio)
+        {
+            return _contexto.Prioridades.AsNoTracking().Where(criterio).ToList();
         }
 
 
