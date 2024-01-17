cf3e79b840278f34fb2ce222530eac2c0af0694d
Yudelka <yudelkaguillen07@gmail.com>
2024-01-17 06:33:20 -0400

    Agregando métodos asíncronos y cambiando nombres

--- BLL/PrioridadesBLL.cs
+++ BLL/PrioridadesBLL.cs
@@ -2,6 +2,7 @@
 using ProyectoManager.DAL;
 using ProyectoManager.Models;
 using System.Linq.Expressions;
+using System.Runtime.InteropServices;
 
 namespace ProyectoManager.BLL
 {
@@ -13,89 +14,72 @@ namespace ProyectoManager.BLL
         {
             _contexto = contexto;
         }
-
-        public bool Save(Prioridades Prioridades)
+        public async Task<bool> Guardar(Prioridades prioridad)
         {
-            if(Validar(Prioridades))
-            {
-                return false;
-            }
-            if (Prioridades.PrioridadesId == 0)
-                _contexto.Prioridades.Add(Prioridades);
+            if (!await Existe(prioridad.PrioridadesId))
+                return await Insertar(prioridad);
             else
-                _contexto.Entry(Prioridades).State = EntityState.Modified;
-
-            var saved = _contexto.SaveChanges() > 0;
-            return saved;
+                return await Modificar(prioridad);
         }
 
-        public bool Validar(Prioridades prioridades)
+        private async Task<bool> Insertar(Prioridades prioridades)
         {
-            bool encontrado = (_contexto.Prioridades.Any(p => p.Descripcion!.ToLower()
+            _contexto.Prioridades.Add(prioridades);
+            return await _contexto.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Modificar(Prioridades prioridades)
+        {
+            _contexto.Update(prioridades);
+            return await _contexto.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Validar(Prioridades prioridades)
+        {
+             var encontrado =  (_contexto.Prioridades.Any(p => p.Descripcion!.ToLower()
             == prioridades.Descripcion!.ToLower()));
 
             return encontrado;
         }
-        private bool Insert(Prioridades prioridades)
-        {
-            _contexto.Prioridades.Add(prioridades);
-            return _contexto.SaveChanges() > 0;
-        }
-        public async Task<bool> DeleteAsync(int id)
-        {
-            var prioridad = await _contexto.Prioridades.FindAsync(id);
 
-            if (prioridad == null)
-            {
-                return false;
-            }
-            _contexto.Prioridades.Remove(prioridad);
-            var eliminado = await _contexto.SaveChangesAsync() > 0;
+        public async Task<bool> Eliminar(Prioridades prioridades)
+        {
+            var cantidad = await _contexto.Prioridades
+                .Where(o => o.PrioridadesId == prioridades.PrioridadesId)
+                .ExecuteDeleteAsync();
 
-            // si no hay registros en la tabla
-            var totalRegistros = await _contexto.Prioridades.CountAsync();
-            if (totalRegistros == 0)
-            {
-                // borraa, para que no tenga el numero siguiente al orden si se borra todos los registros
-                await _contexto.Database.ExecuteSqlRawAsync("DELETE FROM sqlite_sequence WHERE name='Prioridades'");
-            }
-            return eliminado;
+            return cantidad > 0;
         }
 
-        public bool Modify(Prioridades prioridades)
+        public async Task<Prioridades?> Buscar(int prioridadesId)
         {
-            _contexto.Update(prioridades);
-            int cant = _contexto.SaveChanges();
-            return cant > 0;
-        }
-        
-        public bool Exist(int prioridadesId)
-        {
-            return _contexto.Prioridades.Any(o => o.PrioridadesId == prioridadesId);
-
+            return await _contexto.Prioridades
+                .AsNoTracking()
+                .FirstOrDefaultAsync(o => o.PrioridadesId == prioridadesId);
         }
 
-      
-        public async Task<Prioridades?> FindAsync(int id)
+
+        public async Task<List<Prioridades>> Listar(Expression<Func<Prioridades, bool>> criterio)
         {
-            return await _contexto.Prioridades.FindAsync(id);
+            return await _contexto.Prioridades.AsNoTracking().Where(criterio).ToListAsync();
         }
 
-        public async Task <Prioridades?> Buscar(int prioridadesId)
+        public async Task<bool> Existe(int prioridadesId)
         {
-            return _contexto.Prioridades.Where(o => o.PrioridadesId
-            == prioridadesId).AsNoTracking().FirstOrDefault(o => o.PrioridadesId == prioridadesId); 
-        }
+            return await _contexto.Prioridades
+                .AnyAsync(p => p.PrioridadesId == prioridadesId);
 
-        public List<Prioridades> GetList(Expression<Func<Prioridades, bool>> criterio)
-        {
-            return _contexto.Prioridades.AsNoTracking().Where(criterio).ToList();
         }
 
         public List<Prioridades> GetPrioridades()
         {
             var prioridades = _contexto.Prioridades.ToList();
             return prioridades;
+        }
+
+        public async Task<Prioridades?> FindAsync(int id)
+        {
+            return await _contexto.Prioridades.FindAsync(id);
         }
     }
 }
