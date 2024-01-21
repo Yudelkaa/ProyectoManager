d47c6f3dbe3b0e506c5136cff4e87c220247c121
Yudelka <yudelkaguillen07@gmail.com>
2024-01-20 22:54:09 -0400

    Interfaz, métodos y validaciones

--- Models/Clientes.cs
+++ Models/Clientes.cs
@@ -8,14 +8,23 @@ namespace ProyectoManager.Models
     {
         [Key]
         public int ClientesId { get; set; }
+
         [Required(ErrorMessage = "Ingrese su nombre")]
         public string? Nombres { get; set; }
+
+
         [Required(ErrorMessage = "Por favor, ingrese su número de contacto")]
-        public int Celular { get; set; }
+        public long Celular { get; set; }
+
+
         [RegularExpression(@"^\d{3}-\d{7}-\d$", ErrorMessage = "Por favor, ingrese su RNC")]
-        public int RNC { get; set; }
+        public string? RNC { get; set; }
+
+
         [Required(ErrorMessage = "Por favor, complete el campo")]
         public string? Email { get; set; }
+
+
         [Required(ErrorMessage = "Por favor, ingrese su dirección")]
         public string? Direccion { get; set; }
     } 
