74d1ebc40eb19d63265a9d20af0ce15cf241168a
Yudelka <yudelkaguillen07@gmail.com>
2024-01-28 18:22:22 -0400

    Contexto :D

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -7,6 +7,8 @@ namespace ProyectoManager.DAL
     {
         public DbSet<Prioridades> Prioridades { get; set; }
         public DbSet<Clientes> Clientes { get; set; }
+
+        public DbSet<Tickets> Tickets { get; set; }
         public Contexto(DbContextOptions<Contexto> Options): base(Options) { }
     }
 }
