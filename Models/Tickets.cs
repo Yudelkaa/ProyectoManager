4652067045f82db33270cd87a4a7156bd2541b76
Yudelka <yudelkaguillen07@gmail.com>
2024-01-28 19:48:09 -0400

    Validaciones

--- Models/Tickets.cs
+++ Models/Tickets.cs
@@ -1,20 +1,32 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoManager.Models
 {
-	
 	public class Tickets
 	{
 		[Key]
 		public int TicketId { get; set; }
-		public DateTime Fecha {  get; set; }
-		public int ClienteId { get; set;}
-		public int SistemaId {  get; set; }
-		public int PrioridadId { get; set;}
-		public string? SolicitadoPor { get; set;}
 
-		public string? Asunto { get; set;}
+		[Required(ErrorMessage = "Es requerido")]
+		public DateTime Fecha { get; set; }
+
+		[Required(ErrorMessage = "Es requerido")]
+		public int ClienteId { get; set; }
+
+		[Range(1, int.MaxValue, ErrorMessage = "Debe ser válido")]
+		public int SistemaId { get; set; }
+
+		[Range(1, int.MaxValue, ErrorMessage = "Debe ser válido")]
+		public int PrioridadId { get; set; }
+
+		[Required(ErrorMessage = "Es requerido")]
+		public string? SolicitadoPor { get; set; }
+
+		[Required(ErrorMessage = "Es requerido")]
+		public string? Asunto { get; set; }
+
+		[Required(ErrorMessage = "Es requerido")]
 		public string? Descripcion { get; set; }
-
 	}
 }
