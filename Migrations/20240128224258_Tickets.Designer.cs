eb8bce895cfc8c5a8393c839a7dc6b8799a97b58
Yudelka <yudelkaguillen07@gmail.com>
2024-01-28 19:00:56 -0400

    Migración

--- Migrations/20240128224258_Tickets.Designer.cs
+++ Migrations/20240128224258_Tickets.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProyectoManager.DAL;
 
@@ -10,9 +11,11 @@ using ProyectoManager.DAL;
 namespace ProyectoManager.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20240128224258_Tickets")]
+    partial class Tickets
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.1");
