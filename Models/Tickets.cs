a67f2189841764b37456e3d07bbde6daec2fe3f7
Yudelka <yudelkaguillen07@gmail.com>
2024-01-28 18:19:40 -0400

    Modelo Tickets :D

---
+++ Models/Tickets.cs
@@ -0,0 +1,16 @@
+﻿namespace ProyectoManager.Models
+{
+	public class Tickets
+	{
+		public int TicketId { get; set; }
+		public DateTime Fecha {  get; set; }
+		public int ClienteId { get; set;}
+		public int SistemaId {  get; set; }
+		public int PrioridadId { get; set;}
+		public string? SolicitadoPor { get; set;}
+
+		public string? Asunto { get; set;}
+		public string? Descripcion { get; set; }
+
+	}
+}
