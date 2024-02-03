4c7d164a28b0aeed059aa8e6e88a36459fdb3102
Yudelka <yudelkaguillen07@gmail.com>
2024-02-03 12:04:30 -0400

    Cambios de validaciones finales

--- Models/Clientes.cs
+++ Models/Clientes.cs
@@ -12,14 +12,15 @@ namespace ProyectoManager.Models
         [Required(ErrorMessage = "Ingrese su nombre")]
         public string? Nombres { get; set; }
 
-        [Required(ErrorMessage = "Ingrese el número celular correctamente")]
-        public long Celular { get; set; }
+		[RegularExpression(@"^[0-9]{10}$", ErrorMessage = "Ingrese un número de celular válido")]
+		public long Celular { get; set; }
 
-        [Required(ErrorMessage = "Ingrese el teléfono correctamente")]
-        public long Telefono { get; set; }
+		[RegularExpression(@"^[0-9]{10}$", ErrorMessage = "Ingrese un número de teléfono válido")]
+		public long Telefono { get; set; }
 
-        [Required(ErrorMessage = "Por favor, ingrese el RNC correctamente")]
-        public long RNC { get; set; }
+		[RegularExpression(@"^\d{9}$|^\d{11}$", ErrorMessage = "Ingrese un RNC válido de 9 o 11 dígitos")]
+		[Required(ErrorMessage = "Por favor, ingrese el RNC correctamente")]
+		public long RNC { get; set; }
 
         [Required(ErrorMessage = "Por favor, complete el campo")]
         public string? Email { get; set; }
